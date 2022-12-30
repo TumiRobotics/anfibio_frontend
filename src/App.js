c120fd57ef3b3bdc41886cc16bc12c032b52d294
Jaime Del Alcazar <jdelalcazar@utec.edu.pe>
2022-12-30 10:50:29 -0500

    Se modifico script de electron

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './components/Home';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { createBrowserHistory, createHashHistory } from 'history';
 import { isElectron } from './utils';
 
